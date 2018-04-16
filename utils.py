84cf49b22d3c345ec3cd6ffcc2c6efd17597b0b7
Ryan Lambert <ryan.g.lambert@gmail.com>
2018-04-16 15:45:38 -0400

    partial submission

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+
+from sklearn.linear_model import RANSACRegressor
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline
 # from statsmodels.tsa.stattools import adfuller
 
 
@@ -25,8 +29,40 @@ import pandas as pd
 #         dfoutput['Critical Value (%s)' % key] = value
 #     print(dfoutput)
 
+def WMAE(y_true, y_pred, weights=None):
+    if weights is None:
+        # don't apply weights
+        return np.mean(np.abs(y_true - y_pred))
+    # do apply weights
+    return np.mean(np.abs(y_true - y_pred) * weights)
 
-def WMAE(y_pred, y_true, weights=None):
-    if weights:
-        return np.mean(np.abs(y_pred - y_true) * weights)
-    return np.mean(np.abs(y_pred - y_true))
+
+def cross_validate(model, X, Y, w, K=3):
+    cv = KFold(K, shuffle=True, random_state=42)
+
+    cross_val_mae = []
+    for train, val in cv.split(X, Y):
+        model = Pipeline(steps=[('minmax_scaler', MinMaxScaler()),
+                                ('RANSAC', RANSACRegressor())])
+        model.fit(X.iloc[train],
+                  Y.iloc[train])
+        y_pred = model.predict(X.iloc[val])
+        cross_val_mae.append(WMAE(Y.iloc[val].values, y_pred, w.iloc[val].values))
+
+    print('Cross Val Score: {}\n Cross Val Std: {}\n K: {}'.format(np.mean(cross_val_mae), np.std(cross_val_mae), K))
+
+
+def resid_plot(y_pred, y_actual, title):
+    fig = plt.figure(figsize=(8, 4))
+    ax1 = fig.add_subplot(1, 2, 1)
+    ax1.scatter(y_pred, y_pred - y_actual)
+    ax1.set_xlabel('Predicted')
+    ax1.set_ylabel('Predicted - Actual')
+    plt.title(title + " Residuals", fontsize=10)
+    ax2 = fig.add_subplot(1, 2, 2)
+    ax2.hist(y_pred - y_actual, orientation='horizontal', bins=15)
+    plt.title(title + " Hist of Residuals", fontsize=10)
+#     plt.savefig('/Users/ryanlambert/Desktop/' + title + "_Hist_of_Residuals.png")
+    plt.show()
+    plt.hexbin(y_pred, y_pred - y_actual, gridsize=20)
+    plt.title(title + " Hexbin of Residuals", fontsize=10)
